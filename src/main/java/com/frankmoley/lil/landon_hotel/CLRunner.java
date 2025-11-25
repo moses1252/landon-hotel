9eb5d495aa0a352c041fdd0da98104704721c5ed
moses1252 <amviramontes34@yahoo.com>
2025-11-25 13:19:13 -0800

    i connected my spring boot application to a postressql database, and its functional!

--- src/main/java/com/frankmoley/lil/landon_hotel/CLRunner.java
+++ src/main/java/com/frankmoley/lil/landon_hotel/CLRunner.java
@@ -6,8 +6,6 @@ import com.frankmoley.lil.landon_hotel.data.entity.Room;
 import com.frankmoley.lil.landon_hotel.data.repository.GuestRepository;
 import com.frankmoley.lil.landon_hotel.data.repository.ReservationRepository;
 import com.frankmoley.lil.landon_hotel.data.repository.RoomRepository;
-import com.frankmoley.lil.landon_hotel.service.RoomReservationService;
-import com.frankmoley.lil.landon_hotel.service.model.RoomReservation;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,23 +14,26 @@ import java.util.Optional;
 
 @Component
 public class CLRunner implements CommandLineRunner {
-
     private final RoomRepository roomRepository;
     private final GuestRepository guestRepository;
     private final ReservationRepository reservationRepository;
-    private final RoomReservationService roomReservationService;
 
-    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository,
-                    ReservationRepository reservationRepository, RoomReservationService roomReservationService) {
+    public CLRunner(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
         this.guestRepository = guestRepository;
         this.reservationRepository = reservationRepository;
-        this.roomReservationService = roomReservationService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        List<RoomReservation> reservations = this.roomReservationService.getRoomReservationsForDate("2023-08-28");
+        System.out.println("*** GUESTS ***");
+        List<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        System.out.println("*** ROOMS ***");
+        List<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+        System.out.println("*** RESERVATIONS ***");
+        List<Reservation> reservations = this.reservationRepository.findAll();
         reservations.forEach(System.out::println);
     }
 
--- src/main/java/com/frankmoley/lil/landon_hotel/data/repository/RoomRepository.java
+++ src/main/java/com/frankmoley/lil/landon_hotel/data/repository/RoomRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    Optional<Room> findByRoomNumberIngnoreCase(String roomNumber);
+    Optional<Room> findByRoomNumberIgnoreCase(String roomNumber);
 }
